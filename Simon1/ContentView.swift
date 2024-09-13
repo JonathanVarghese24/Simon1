58df788e4246d481aef6a826aeeb68e2b2065ebe
JonathanVarghese <jvarghese@bsd220.org>
2024-09-13 12:39:15 -0500

    Added 4 squares, changed simon size

--- Simon1/ContentView.swift
+++ Simon1/ContentView.swift
@@ -8,18 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var colorDisplay = [ColorDisplay(color: .green),
+                                       ColorDisplay(color: .red),
+                                       ColorDisplay(color: .yellow),
+                                       ColorDisplay(color: .blue)]
+    
     var body: some View {
-        VStack {
-            Text("Simon")
-                .font(.system(size: 72))
-            
+        ZStack {
+            VStack {
+                Text("Simon")
+                    .font(.system(size: 45))
+            }
+            VStack {
+                HStack {
+                    colorDisplay[0]
+                    colorDisplay[1]
+                }
+                HStack {
+                    colorDisplay[2]
+                    colorDisplay[3]
+                }
+            }
+            .preferredColorScheme(.dark)
         }
-        
-        .padding()
     }
+}
+
+struct ColorDisplay: View {
+    let color: Color
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25.0)
+            .fill(color)
+            .frame(width: 100, height: 100, alignment: .center)
+            .padding()
+    }
+    
 }
 
 #Preview {
     ContentView()
-        .preferredColorScheme(.dark)
 }
