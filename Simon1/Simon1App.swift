ddba780e60b9d01b6c1b11ee1d92bdc63a525c08
JonathanVarghese <jvarghese@bsd220.org>
2024-09-13 12:21:31 -0500

    Initial Commit

---
+++ Simon1/Simon1App.swift
@@ -0,0 +1,17 @@
+//
+//  Simon1App.swift
+//  Simon1
+//
+//  Created by Jonathan V on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Simon1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
