afd55d723186e0a917f1160bf3e58bda2bf2aae5
JonathanVarghese <jvarghese@bsd220.org>
2024-09-13 12:30:17 -0500

    Initial UI Done

--- Simon1/ContentView.swift
+++ Simon1/ContentView.swift
@@ -10,15 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Simon")
+                .font(.system(size: 72))
+            
         }
+        
         .padding()
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
